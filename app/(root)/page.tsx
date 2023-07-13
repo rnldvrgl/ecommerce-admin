befd3277bb5216fb465ba81a0fc27997f3ed2d60
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-13 15:20:50 +0800

    add UserButton

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+UserButton
 
 const SetupPage = () => {
     return (
         <div className="p-4">
             This is a protected route!
+            <UserButton afterSignOutUrl="/" />
         </div >
     )
 }
