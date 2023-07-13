dd3628b58c80a3dda246c74cfa4a94e523e7b92b
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-13 14:42:13 +0800

    done environment setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div className="p-4">
+            <p>Hello Admin Dashboard</p>
+            <Button size="default" variant="outline">Click Me</Button>
+        </div >
+    )
+}
