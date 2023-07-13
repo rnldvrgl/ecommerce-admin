0e228470414978d77fd12dfa75577f60d5ae4bfd
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-13 18:00:42 +0800

    add use state for loading in store modal

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -9,6 +9,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "../ui/button"
+import { useState } from "react"
 
 const formSchema = z.object({
     name: z.string().min(1)
@@ -16,6 +17,8 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+
+    const [loading, setLoading] = useState(false);
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
