9e2be359c549c30a4747564d7e57bd3b56e863fe
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-14 15:37:22 +0800

    add heading component

---
+++ components/ui/heading.tsx
@@ -0,0 +1,16 @@
+interface HeadingProps {
+    title: string,
+    description: string,
+}
+
+export const Heading: React.FC<HeadingProps> = ({
+    title,
+    description
+}) => {
+    return (
+        <div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    )
+}
