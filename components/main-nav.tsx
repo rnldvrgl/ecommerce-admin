cd03d9479803160ed83dad8e7fa1d2c6aec26492
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-14 17:23:38 +0800

    update main-nav route for overview

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -12,15 +12,22 @@ export function MainNav({
     const pathname = usePathname();
     const params = useParams();
 
-    const routes = [{
-        href: `/${params.storeId}/settings`,
-        label: 'Settings',
-        active: pathname === `/${params.storeId}/settings`,
-    }];
+    const routes = [
+        {
+            href: `/${params.storeId}`,
+            label: 'Overview',
+            active: pathname === `/${params.storeId}`,
+        },
+        {
+            href: `/${params.storeId}/settings`,
+            label: 'Settings',
+            active: pathname === `/${params.storeId}/settings`,
+        }
+    ];
 
 
     return (
-        <nav className={cn("flex items - center space - x - 4 lg: space - x - 6", className)}>
+        <nav className={cn("flex items - center space-x-4 lg: space-x -6", className)}>
             {routes.map((route) => (
                 <Link key={route.href} href={route.href} className={cn("text-sm font-medium transition-colors hover:text-primary", route.active ? "text-black dark:text-white" : "text-muted-foreground")}>
                     {route.label}
