2ce35602d11a06f75868ff6ce439910060479c28
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-07-14 23:04:13 +0800

    Update settings-form.tsx

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -17,6 +17,7 @@ import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -34,6 +35,8 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 
     const router = useRouter();
     const params = useParams();
+    const origin = useOrigin();
+
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
 
