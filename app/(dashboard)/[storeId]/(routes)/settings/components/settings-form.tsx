69f19de19856f104910eb308fb01afefd562f2c2
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-07-14 22:29:45 +0800

    Update settings-form.tsx

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -121,7 +121,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
             <Separator />
             <ApiAlert
                 title="NEXT_PUBLIC_API_URL"
-                description="test-desc"
+                description={`${origin}/api/${params.storeId}`}
                 variant="public"
             />
         </>
