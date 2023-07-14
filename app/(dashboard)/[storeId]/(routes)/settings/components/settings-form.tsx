382f03dd81254d1972cbcf0eddfd10b6a1a9fd8c
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-14 15:46:52 +0800

    install shad cn separator

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -14,18 +14,21 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
     return (
-        <div className="flex items-center justify-between">
-            <Heading
-                title="Settings"
-                description="Manage store preferences"
-            />
-            <Button
-                variant="destructive"
-                size="sm"
-                onClick={() => { }}
-            >
-                <Trash className="h-4 w-4" />
-            </Button>
-        </div>
+        <>
+            <div className="flex items-center justify-between">
+                <Heading
+                    title="Settings"
+                    description="Manage store preferences"
+                />
+                <Button
+                    variant="destructive"
+                    size="sm"
+                    onClick={() => { }}
+                >
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </div>
+
+        </>
     )
 }
