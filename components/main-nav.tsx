2377d0b987ec89cfe90a3753489ff50685bfd268
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-07-14 23:45:25 +0800

    add initial client.tsx

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -19,10 +19,16 @@ export function MainNav({
             active: pathname === `/${params.storeId}`,
         },
         {
+            href: `/${params.storeId}/billboards`,
+            label: 'Billboards',
+            active: pathname === `/${params.storeId}/billboards`,
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}/settings`,
-        }
+        },
+
     ];
 
 
