332f586c4736be8a8c53f8178a378f607a6187ef
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-17 13:49:01 +0800

    done Column Definitions

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,5 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/columns";
+import { format } from "date-fns";
 
 const BillboardsPage = async ({
     params
@@ -15,10 +17,16 @@ const BillboardsPage = async ({
         }
     })
 
+    const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+        id: item.id,
+        label: item.label,
+        createdAt: format(item.createdAt, "MMMM do, yyyy")
+    }))
+
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient data={billboards} />
+                <BillboardClient data={formattedBillboards} />
             </div>
         </div>
     )
