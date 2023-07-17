29f7fbc307c5c0e8d32a6c6ce72922d122c3b57d
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-17 14:21:22 +0800

    add copy id function

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,6 +1,10 @@
 "use client"
 
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { BillboardColumn } from "./columns"
+import { Button } from "@/components/ui/button"
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react"
+import { toast } from "react-hot-toast"
 
 interface CellActionProps {
     data: BillboardColumn
@@ -9,8 +13,42 @@ interface CellActionProps {
 export const CellAction: React.FC<CellActionProps> = ({
     data
 }) => {
+
+    const onCopy = (id: string) => {
+        navigator.clipboard.writeText(id);
+        toast.success("Billboard ID copied to the clipboard.");
+    }
+
     return (
-        <div>Action</div>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    variant="ghost" className="h-8 w-8 p-0"
+                >
+                    <span className="sr-only">
+                        Open menu
+                    </span>
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>
+                    Actions
+                </DropdownMenuLabel>
+                <DropdownMenuItem>
+                    <Edit className="mr-2 h-4 w-4" />
+                    Update
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => onCopy(data.id)}>
+                    <Copy className="mr-2 h-4 w-4" />
+                    Copy ID
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Trash className="mr-2 h-4 w-4" />
+                    Delete
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
 
