c832cdafa4ec16c83a9da240abd1b344b7fa6df8
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-18 14:09:07 +0800

    minor changes

--- app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { BillboardColumn } from "./columns"
+import { ProductColumn } from "./columns"
 import { Button } from "@/components/ui/button"
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react"
 import { toast } from "react-hot-toast"
@@ -11,7 +11,7 @@ import axios from "axios"
 import { AlertModal } from "@/components/modals/alert-modal"
 
 interface CellActionProps {
-    data: BillboardColumn
+    data: ProductColumn
 }
 
 export const CellAction: React.FC<CellActionProps> = ({
--- app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -6,6 +6,12 @@ import { CellAction } from "./cell-action"
 export type ProductColumn = {
     id: string
     name: string
+    price: string
+    size: string
+    category: string
+    color: string
+    isFeatured: boolean
+    isArchived: boolean
     createdAt: string
 }
 
@@ -13,6 +19,39 @@ export const columns: ColumnDef<ProductColumn>[] = [
     {
         accessorKey: "name",
         header: "Name",
+    },
+    {
+        accessorKey: "isArchived",
+        header: "Archived",
+    },
+    {
+        accessorKey: "isFeatured",
+        header: "Featured",
+    },
+    {
+        accessorKey: "price",
+        header: "Price",
+    },
+    {
+        accessorKey: "category",
+        header: "Category",
+    },
+    {
+        accessorKey: "size",
+        header: "Size",
+    },
+    {
+        accessorKey: "color",
+        header: "Color",
+        cell: ({ row }) => (
+            <div className="flex items-center gap-x-2">
+                {row.original.color}
+                <div
+                    className="h-6 w-6 rounded-full border"
+                    style={{ backgroundColor: row.original.color }}
+                />
+            </div>
+        )
     },
     {
         accessorKey: "createdAt",
