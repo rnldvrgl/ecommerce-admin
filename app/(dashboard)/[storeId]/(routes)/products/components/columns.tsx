ebdc68fb4e4d49b579a7004417791ac49ea55525
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-07-18 13:55:13 +0800

    minor changes

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { CellAction } from "./cell-action"
+
+export type ProductColumn = {
+    id: string
+    name: string
+    createdAt: string
+}
+
+export const columns: ColumnDef<ProductColumn>[] = [
+    {
+        accessorKey: "name",
+        header: "Name",
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Date",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => <CellAction data={row.original} />
+    }
+]
